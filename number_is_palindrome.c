bd41daab9cbd3cfa54324d71a0e227606ef6399b
Jacob <jjalex87@users.noreply.github.com>
2020-02-08 01:22:10 +0530

    Create number_is_palindrome.c

---
+++ number_is_palindrome.c
@@ -0,0 +1,2 @@
+Let the given number be num. A simple method for this problem is to first reverse digits of num, then compare the reverse of num with num. 
+If both are same, then return true, else false.
